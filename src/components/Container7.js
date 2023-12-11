96c324c5133130d63b3f8e6d42a33d522a4ebf8b
Nada Smith <ayronadas@gmail.com>
2023-12-11 05:41:43 -0500

    Built container 7

---
+++ src/components/Container7.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Container7 = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                Most versatile
+                            </div>
+                        </div>
+                        <div>
+                            Create multiple chatbots for different purposes
+                        </div>
+                        <div>
+                            Legal documents? Summarising a book? Extracting data from a website? We allow you to create multiple chatbots at one time, so you can use Afforai for a whole range of purposes.
+                        </div>
+                        <div>
+                            <button type="button">Get started</button>
+                            <button type="button">View pricing</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <picture>
+                        <source srcSet="/images/laptop-landing-11.png" type="image/webp" />
+                        <img src="/images/laptop-landing-11.png" alt="Afforai masters the documents you upload" />
+                    </picture>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Container7;
