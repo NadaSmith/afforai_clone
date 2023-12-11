cc8679ba33d7d76198d534f1ac9cbd9f2de01df5
Nada Smith <ayronadas@gmail.com>
2023-12-11 09:56:44 -0500

    Built out container 15.

--- src/components/Container15.js
+++ src/components/Container15.js
@@ -3,7 +3,24 @@ import React from "react";
 const Container15 = () => {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            Start saving time today!
+                        </div>
+                        <div>
+                            Join an ever-growing community, and get with Afforai started free of charge!
+                        </div>
+                        <button type="button">
+                            Get Started for Free
+                        </button>
+                    </div>
+                </div>
+                <div>
+                    <img src="/images/laptop-landing-16.png" />
+                </div>
+            </div>
         </div>
     );
 };
