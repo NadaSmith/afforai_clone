3ca07b0ee24e1936ee9c93b40f6caeea0b773c18
Nada Smith <ayronadas@gmail.com>
2023-12-10 20:11:56 -0500

    Built out component3

--- src/components/Containter3.js
+++ src/components/Containter3.js
@@ -3,7 +3,12 @@ import React from "react";
 const Container3 = () => {
     return (
         <div>
-            
+            <div>
+                Loved by <span>20,000+</span> users around the world
+                <div>
+                    <img src="image/laptop-landing-2.png"> </img>
+                </div>
+            </div>
         </div>
     );
 };
