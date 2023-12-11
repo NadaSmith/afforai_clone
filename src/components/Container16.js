18544bc676c2d0e87cc849919d375004559924ad
Nada Smith <ayronadas@gmail.com>
2023-12-11 10:24:06 -0500

    Started the build out on container 16; need to add javascript

---
+++ src/components/Container16.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+const Container16 = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            FAQs
+                        </div>
+                        <div>
+                            For more information, check out our
+                            <a href="https://help.afforai.com/">
+                                Help Center
+                            </a>
+                            .
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    Is Afforai free?
+                                </div>
+                                <div>
+                                    
+                                </div>
+                            </div>
+                            <div>
+                                Yes, Afforai is free for all new users. Upon signing up, users are given 50 permanent credits
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    What are subscription credits versus permanent credits?
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                            <div>
+                                Subscription credits come from monthly payments, while permanent credits come from top ups. Subscription credits will be used before permanent credits, and subscription credits expire every month while permanent credits stay.
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    Does Afforai support documents storing on the cloud like Google Drive, One Drive, or Drop Box?
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                            <div>
+                                Currently Afforai only support uploading local documents and websites, in the near future we will integrate these cloud solutions. Sign up with us to get updated.
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    Does Afforai support uploading images and video?
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                            <div>
+                                Currently Afforai supports OCR for scanned PDF, but not via image file directly. Afforai currently working on working with video and audio transcript.
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    Does Afforai support collaboration between accounts?
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                            <div>
+                                Currently Afforai is working on a solution to allow users to share chatbots and files. Sign up with us to get updated when this feature roll out.
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    Is my data secred?
+                                </div>
+                                <div>
+
+                                </div>
+                            </div>
+                            <div>
+                                Yes your data is secured with Afforai. We don't store your conversation with the AI, and the files you uploaded on the system is stored on the cloud using Microsoft Azure and MongoDB with their standard security. LLM calls are made using OpenAI Azure with their security measures.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Container16;
