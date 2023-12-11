a4758ba0caf7006e76ab2fd0510b41c489edd7b9
Nada Smith <ayronadas@gmail.com>
2023-12-11 10:47:24 -0500

    Built out container 17 and removed extra containers.

--- src/components/Container17.js
+++ src/components/Container17.js
@@ -3,7 +3,77 @@ import React from "react";
 const Container17 = () => {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    <picture>
+                                        <source srcSet="/images/logo-new-violet.webp" type="image/webp" />
+                                        <img src="/images/logo-new-violent.png" alt="Afforai Logo" />
+                                    </picture>
+                                    <div>
+                                        Afforai
+                                    </div>
+                                </div>
+                                <div>
+                                    Afforai is your research tool in to damn near anything. Helping you summarise, translate, and withdraw data from documents.
+                                </div>
+                                <div>Copyright © 2023 Afforai Inc. All rights reserved.</div>
+                            </div>
+                            <a href="https://www.producthunt.com/posts/afforai?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-afforai">
+                                <img src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=403202&theme=light&period=daily" alt="Afforai - Summarize, interact, translate your documents with AI | Product Hunt" />
+                            </a>
+                            <div>
+                                <div>
+                                    Backed by
+                                </div>
+                                <img src="/images/sputnikatx.png" alt="Sputnik A TX VC" />
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                Company
+                            </div>
+                            <a href="https://www.afforai.com/our_story">Our Story</a>
+                            <a href="https://afforai.getrewardful.com/signup">Affiliate</a>
+                            <a href="https://www.afforai.com/security">Security</a>
+                            <a href="https://trello.com/b/98yNMfwA/afforai-roadmap">Roadmap</a>
+                            <a href="https://www.afforai.com/contact">Contact Us</a>
+                        </div>
+                        <div>
+                            <div>
+                                Resources
+                            </div>
+                            <a href="https://www.afforai.com/documentation">Help Center</a>
+                            <a href="https://www.afforai.com/documentation">API Docs</a>
+                            <a href="https://www.afforai.com/terms_of_use">Terms of Use</a>
+                            <a href="https://www.afforai.com/privacy_policy">Privacy Policy</a>
+                        </div>
+                        <div>
+                            <div>
+                                Follow Us
+                            </div>
+                            <a href="https://www.facebook.com/groups/afforai">Facebook Group</a>
+                            <a href="https://twitter.com/afforai/">LinkedIn</a>
+                            <a href="https://twitter.com/afforai/">Twitter</a>
+                            <a href="https://medium.com/@alec-afforai">Medium Blog</a>
+                        </div>
+                        <div>
+                            <div>
+                                Language
+                            </div>
+                            <div>
+                                English
+                            </div>
+                            <div>
+                                Vietnamese
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
