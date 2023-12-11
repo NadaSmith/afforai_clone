04b94754ec93cd898d894cc960b5ede3d8bde1ff
Nada Smith <ayronadas@gmail.com>
2023-12-11 06:29:33 -0500

    Built out container 11.

--- src/components/Container11.js
+++ src/components/Container11.js
@@ -3,7 +3,26 @@ import React from "react";
 const Container11 = () => {
     return (
         <div>
-            
+            <div>       
+                <div>
+                    <div>
+                        <div>
+                            Seamless Integrations - Coming Soon
+                        </div>
+                        <div>
+                            Upload Files directly from Google Drive, One Drive &amp Dropbox
+                        </div>
+                        <div>
+                            Afforai allows you to upload files directly from Google Drive, One Drive &amp; Dropbox, meaning your experience will be smooth sailing regardless of your preferred 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/laptop-landing-15.png" alt="Click here to upload your document" />
+                </div>
+            </div>
         </div>
     );
 };
