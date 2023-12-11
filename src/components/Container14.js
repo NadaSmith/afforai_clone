ad79ef8903717e72a48fed9a182b5012857ddc15
Nada Smith <ayronadas@gmail.com>
2023-12-11 09:44:59 -0500

    Built out container14

--- src/components/Container14.js
+++ src/components/Container14.js
@@ -3,7 +3,29 @@ import React from "react";
 const Container14 = () => {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                My promise to you
+                            </div>
+                            <div>
+                                % monehy back guarnateed if you are not satisfied
+                            </div>
+                            <div>
+                                We crafted Afforai with total belief in our ability to make a chatbot that is not only powerful and reliable, but able to maximise your satisfaction as a user. This is why we have implemented a money back guarantee if you find yourself not satisfied with the capabilities of Afforai.
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <picture>
+                            <source srcSet="/images/laptop-landing-20.png" type="image/webp" />
+                            <img src="/images/laptop-landing-20.png" alt="100% money back gaurantee" />
+                         </picture>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
