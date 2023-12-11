d5810300289728f7ad52ad3a0babb84388382291
Nada Smith <ayronadas@gmail.com>
2023-12-11 06:13:24 -0500

    Built out container 9

---
+++ src/components/Container9.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Container9 = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            Multiple Language Supported
+                        </div>
+                        <div>
+                            Regardless of where you are from, Afforai is for you
+                        </div>
+                        <div>
+                            We support more than 100 Languages, meaning you can seamlessly use our services regardless of where you are from. Our Cross Language Querying feature also allows flawless translation of documents.
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <picture>
+                        <source srcSet="/images/laptop-landing-13.png" type="image/webp"></source>
+                        <img src="/images/laptop-landing-13.png" alt="Afforai supports over 100 languages and allow users to upload and ask question in most languages" />
+                    </picture>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Container9;
