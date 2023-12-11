ab6c4293a112e924c122130f2452441fecf4c471
Nada Smith <ayronadas@gmail.com>
2023-12-10 20:45:52 -0500

    Built out container 5

---
+++ src/components/Container5.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Container5 = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>10x your productivity</div>
+                        </div>
+                        <div>
+                            Save yourself from stress & streamline your workflow
+                        </div>
+                        <div>
+                            The average worker spends 9 hours per week looking through &amp; gathering information from thick stacks of documents. With Afforai, you can save yourself 8 hours per week (plus a lot of headaches).
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="/images/laptop-landing-5.png" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Container5;
