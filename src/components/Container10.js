ed88e67d71209e349c9fb145fb0c207d694474db
Nada Smith <ayronadas@gmail.com>
2023-12-11 06:24:28 -0500

    Built out container 10

--- src/components/Container10.js
+++ src/components/Container10.js
@@ -3,7 +3,27 @@ import React from "react";
 const Container10 = () => {
     return (
         <div>
-           
+           <div>
+                <div>   
+                    <div>
+                        <div>
+                            Get more out of your files
+                        </div>
+                    </div>
+                    <div>
+                        File Wizard - a game changing new feature
+                    </div>
+                    <div>
+                        Convert your files with less effort than ever. Afforai’s File Wizard feature allows you to transform your files, in limitless different ways. We can translate, summarize, or explain your upload in no time.
+                    </div>
+                </div>
+                <div>
+                    <picture>
+                        <source srcSet="/images/laptop-landing-14.png" type="image/webp" />
+                        <img src="/images/laptop-landing-14.png" alt="Translate, convert, summarize your files with File Wizard" />
+                    </picture>
+                </div>
+           </div>
         </div>
     );
 };
