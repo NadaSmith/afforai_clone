7d7ee39dc99d7536b38e67a8bc97c2818f784e64
Nada Smith <ayronadas@gmail.com>
2023-12-11 23:41:34 -0500

    Styled container 7

--- src/components/Container7.js
+++ src/components/Container7.js
@@ -1,31 +1,32 @@
 import React from "react";
+import styles from "../Styles/Container7.module.css"
 import landingImage11 from "../Images/mobile-landing-11.png";
 
 const Container7 = () => {
     return (
         <div>
-            <div>
-                <div>
+            <div className={styles.Container7}>
+                <div className={styles.textContent}>
                     <div>
                         <div>
-                            <div>
+                            <div className={styles.box}>
                                 Most versatile
                             </div>
                         </div>
-                        <div>
-                            Create multiple chatbots for different purposes
+                        <div className={styles.title}>
+                            Create multiple chatbots for<br />different purposes
                         </div>
-                        <div>
-                            Legal documents? Summarising a book? Extracting data from a website? We allow you to create multiple chatbots at one time, so you can use Afforai for a whole range of purposes.
+                        <div className={styles.paragraph}>
+                            Legal documents? Summarising a book? Extracting data from a<br />website? We allow you to create multiple chatbots at one time, so you<br />can use Afforai for a whole range of purposes.
                         </div>
-                        <div>
-                            <button type="button">Get started</button>
-                            <button type="button">View pricing</button>
+                        <div className={styles.buttons}>
+                            <button className={styles.button1} type="button">Get started</button>
+                            <button className={styles.button2} type="button">View pricing</button>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <picture>
+                <div className={styles.Image}>
+                    <picture className={styles.imageContainer}>
                         <source srcSet={landingImage11} type="image/webp" />
                         <img src={landingImage11} alt="Afforai masters the documents you upload" />
                     </picture>
