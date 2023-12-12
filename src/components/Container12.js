2fa386a4bec0834ca46e7ad6a12ff9021c67c6ad
Nada Smith <ayronadas@gmail.com>
2023-12-12 01:17:34 -0500

    Finished Styling container 12

--- src/components/Container12.js
+++ src/components/Container12.js
@@ -1,33 +1,34 @@
 import React from "react";
+import styles from "../Styles/Container12.module.css";
 import landingImage17 from "../Images/laptop-landing-17.png";
 import landingImage18 from "../Images/laptop-landing-18.png";
 import landingImage19 from "../Images/laptop-landing-19.png";
 
 const Container12 = () => {
     return (
-        <div>
+        <div className={styles.Container12}>
             <div>
                 <div>
                     <div>
-                        <div>
+                        <div className={styles.title}>
                             Unquestionable accuracy &amp; reliability
                         </div>
-                        <div>
-                            We pride ourselves on the quality of our chatbot's responses, and go the extra mile to ensure the accuracy &amp; reliability of our answers.
+                        <div className={styles.paragraph}>
+                            We pride ourselves on the quality of our chatbot's responses, and go<br />the extra mile to ensure the accuracy &amp; reliability of our answers.
                         </div>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className={styles.imageContainer}>
                 <div>
-                    <div>
+                    <div className={styles.imageContainers}>
                         <div>
-                            <picture>
+                            <picture className={styles.picture}>
                                 <source srcSet={landingImage17} type="image/webp" />
                                 <img src={landingImage17} alt="Security, Reliable, and Trustworthy" />
                             </picture>
                         </div>
-                        <div>
+                        <div className={styles.textContent}>
                             <div>
                                 <span>Trusted by users all over the world</span>
                             </div>
@@ -37,15 +38,15 @@ const Container12 = () => {
                         </div>
                     </div>
                 </div>
-                <div>.ong
-                    <div>
+                <div >
+                    <div className={styles.imageContainers}>
                         <div>
-                            <picture>
+                            <picture className={styles.picture}>
                                 <source srcSet={landingImage18} type="image/webp"/>
                                 <img src={landingImage18} alt="Afforai gives out the best asnwer with most accurate citations" />
                             </picture>
                         </div>
-                        <div>
+                        <div className={styles.textContent}>
                             <div>
                                 <span>Information filtering</span>
                             </div>
@@ -56,14 +57,14 @@ const Container12 = () => {
                     </div>
                 </div>
                 <div>
-                    <div>
+                    <div className={styles.imageContainers}>
                         <div>
-                            <picture>
+                            <picture className={styles.picture}>
                                 <source srcSet={landingImage19} type="image/webp" />
                                 <img src={landingImage19} alt="Afforai AI comprehends documents, not repeating them" />
                             </picture>
                         </div>
-                        <div>
+                        <div className={styles.textContent}>
                             <div>
                                 <span>Built in Comprehension Model</span>
                             </div>
