305f7b8c5588f2b45b7f26a4afa2134bd2f807ce
Nada Smith <ayronadas@gmail.com>
2023-12-12 00:54:01 -0500

    Finished styling container 11

--- src/components/Container11.js
+++ src/components/Container11.js
@@ -1,26 +1,27 @@
 import React from "react";
+import styles from "../Styles/Container11.module.css";
 import landingImage15 from "../Images/laptop-landing-15.png"
 
 const Container11 = () => {
     return (
-        <div>
+        <div className={styles.Container11}>
             <div>       
                 <div>
                     <div>
-                        <div>
+                        <div className={styles.box}>
                             Seamless Integrations - Coming Soon
                         </div>
-                        <div>
-                            Upload Files directly from Google Drive, One Drive &amp; Dropbox
+                        <div className={styles.title}>
+                            Upload Files directly from Google<br />Drive, One Drive &amp; Dropbox
                         </div>
-                        <div>
-                            Afforai allows you to upload files directly from Google Drive, One Drive &amp; Dropbox, meaning your experience will be smooth sailing regardless of your preferred 
+                        <div className={styles.paragraph}>
+                            Afforai allows you to upload files directly from Google Drive, One Drive &amp; Dropbox,<br />meaning your experience will be smooth sailing regardless of your preferred 
                         </div>
                     </div>
                 </div>
             </div>
             <div>
-                <div>
+                <div className={styles.picture}>
                     <img src={landingImage15} alt="Click here to upload your document" />
                 </div>
             </div>
