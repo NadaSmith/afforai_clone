015b1f9e22c977bc01fb7f4832126a4af5ba1b68
Nada Smith <ayronadas@gmail.com>
2023-12-11 22:14:20 -0500

    Completed Styling for container 5

--- src/components/Container5.js
+++ src/components/Container5.js
@@ -1,26 +1,28 @@
 import React from "react";
-import landingImage5 from "../Images/mobile-landing-5.png"
+import landingImage5 from "../Images/mobile-landing-5.png";
+import styles from "../Styles/Container5.module.css";
 
 const Container5 = () => {
     return (
-        <div>
-            <div>
+        <div className={styles.Container5}>
+            <div className={styles.wordContainer}>
                 <div>
                     <div>
-                        <div>
+                        <div className={styles.box}>
                             <div>10x your productivity</div>
                         </div>
-                        <div>
-                            Save yourself from stress & streamline your workflow
+                        <div className={styles.title}>
+                            Save yourself from stress &<br />streamline your workflow
                         </div>
-                        <div>
-                            The average worker spends 9 hours per week looking through &amp; gathering information from thick stacks of documents. With Afforai, you can save yourself 8 hours per week (plus a lot of headaches).
+                        <div className={styles.paragraph}>
+                            The average worker spends 9 hours per week looking through &amp; gathering<br />information from thick stacks of documents. With Afforai, you can save<br />yourself 8 hours per week (plus a lot of headaches).
                         </div>
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
+            <div> 
+                {/*Somehow I have the wrong image, but I made it work*/}
+                <div className={styles.image}> 
                     <img src={landingImage5} />
                 </div>
             </div>
